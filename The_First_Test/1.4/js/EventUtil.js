663283b3607503f85a6e2c34501c1df1b7e7e5d6
Ocean_fish <346966582@qq.com>
2017-07-02 16:32:00 +0800

    2017-07-02 16:31:58

---
+++ The_First_Test/1.4/js/EventUtil.js
@@ -0,0 +1,11 @@
+getEvent:function  (event) {
+	return event?event:window.event;
+}
+getTarget:function  () {
+	return event.target||event/srcElement;
+}
+function function_name () {
+	
+	return event.
+}
+
